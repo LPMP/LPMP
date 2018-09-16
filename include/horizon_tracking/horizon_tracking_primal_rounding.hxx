d84f01efe9c983b53c51b110c1d5f877665f74d2
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2018-09-17 00:00:16 +0200

    printing lb's

--- include/horizon_tracking/horizon_tracking_primal_rounding.hxx
+++ include/horizon_tracking/horizon_tracking_primal_rounding.hxx
@@ -27,6 +27,7 @@ void round_primal_solution(SOLVER& solver, bool send_backward = true)
     auto chain_constructor = solver.template GetProblemConstructor<0>();
     if (send_backward) {
         auto prevLb = solver.GetLP().LowerBound();
+        std::cout<<"Lower bound before send message left: "<<prevLb<<std::endl;
         // for(auto* m : chain_constructor.max_chain_to_graph_messages()) {
         //     m->send_message_to_left(); 
         // }
@@ -41,6 +42,7 @@ void round_primal_solution(SOLVER& solver, bool send_backward = true)
             std::cout<<"New Lower Bound: "<<newLb<<std::endl;
 			throw std::runtime_error("Lower Bound decreased by send backward!");
         }
+        std::cout<<"Lower bound after send message left: "<<newLb<<std::endl;
     }
 
     //TO ADDRESS: Changing this primal pass make primal solution on 5x5 grid test very weak.
