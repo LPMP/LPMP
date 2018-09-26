f1b442fd40c822db2ff924f179c1a4b58c79069e
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2018-09-26 11:26:33 +0200

    Revert "Printing MRF, bottleneck objectives"
    This reverts commit 82ce21eed1b8b1e879f0277e8f5feffb11682afb.

--- src/horizon_tracking/horizon_tracking_fwmap.cpp
+++ src/horizon_tracking/horizon_tracking_fwmap.cpp
@@ -4,40 +4,6 @@
 #include "LP.h"
 #include "LP_FWMAP.hxx"
 using namespace LPMP;
-
-template<typename SOLVER>
-void PrintObjectives(int argc, char** argv, SOLVER& solver)
-{
-    Solver<LP_tree_FWMAP<FMC_HORIZON_TRACKING_MULTIPLE_CHAINS>,StandardVisitor> solver_reference(argc,argv);
-    auto input = horizon_tracking_uai_input::parse_file(solver_reference.get_input_file());
-    construct_horizon_tracking_problem_on_grid_to_chains(input, solver_reference, solver_reference.template GetProblemConstructor<0>());
-    order_nodes_by_label_space_cadinality(solver_reference.template GetProblemConstructor<0>());
-    auto constructor = solver.template GetProblemConstructor<0>();
-    auto constructor_reference = solver_reference.template GetProblemConstructor<0>();
-    auto numberPairwise = constructor_reference.get_number_of_pairwise_factors();
-    assert(numberPairwise == constructor.get_number_of_pairwise_factors());
-    for (std::size_t p = 0; p < numberPairwise; p++) {
-        auto* pairwise_reference = constructor_reference.get_pairwise_factor(p);
-        auto* pairwise = constructor.get_pairwise_factor(p);
-        pairwise_reference->get_factor()->primal() = pairwise->get_factor()->primal();
-        pairwise_reference->propagate_primal_through_messages();
-    }
-    REAL mrf_costs = 0;
-    for (std::size_t u = 0; u < constructor_reference.get_number_of_variables(); u++) {
-        mrf_costs += constructor_reference.get_unary_factor(u)->EvaluatePrimal();
-    }
-    for (std::size_t p = 0; p < constructor_reference.get_number_of_pairwise_factors(); p++) {
-        mrf_costs += constructor_reference.get_pairwise_factor(p)->EvaluatePrimal();
-    }
-    solver_reference.RegisterPrimal();
-    std::cout<<"\n\n MRF Cost: "<<mrf_costs<<std::endl;
-    REAL b_costs = 0;
-    for (const auto& f : constructor_reference.max_multiple_chains_factors()) {
-        b_costs += f->EvaluatePrimal();
-    }
-    std::cout<<"\n\n Bottleneck Cost: "<<b_costs<<std::endl;
-}
-
 int main(int argc, char** argv) {
 Solver<LP_tree_FWMAP<FMC_HORIZON_TRACKING_MULTIPLE_CHAINS>,StandardVisitor> solver(argc,argv);
 auto input = horizon_tracking_uai_input::parse_file(solver.get_input_file());
@@ -57,5 +23,4 @@ round_primal_solution(solver,false);
 solver.WritePrimal();
 std::cout<<"\n\n Primal Cost: "<<solver.primal_cost();
 std::cout<<"\n Percentage duality gap: "<<100.0 * (solver.primal_cost() - solver.lower_bound()) / solver.lower_bound() <<"\%\n\n";
-PrintObjectives(argc, argv, solver);
 }
