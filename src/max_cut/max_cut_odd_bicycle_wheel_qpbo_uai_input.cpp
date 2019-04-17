5b31f5de556a9632907c040904b52c0e779590f8
Paul Swoboda <pswoboda@mpi-inf.mpg.de>
2019-04-17 09:43:29 +0200

    Added executables for qpbo/max-cut.

---
+++ src/max_cut/max_cut_odd_bicycle_wheel_qpbo_uai_input.cpp
@@ -0,0 +1,12 @@
+
+#include "max_cut/max_cut.h"
+#include "visitors/standard_visitor.hxx"
+
+#include "mrf/mrf_uai_input.h"
+using namespace LPMP;
+int main(int argc, char** argv) {
+ProblemConstructorRoundingSolver<Solver<LP<FMC_ODD_BICYCLE_WHEEL_MAX_CUT>,StandardTighteningVisitor>> solver(argc,argv);
+auto input = LPMP::binary_MRF_uai_input::parse_file(solver.get_input_file());
+solver.GetProblemConstructor().construct(input);
+return solver.Solve();
+}
