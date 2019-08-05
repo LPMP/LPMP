79c8d62b1a795d334958234c29b7fb73b5f17108
Fanlin Wang <mywfl@outlook.com>
2019-08-05 10:12:05 +0200

    Add a wrapper class for constructing atomic graph.

--- src/multicut/multicut_cycle_packing_parallel.cpp
+++ src/multicut/multicut_cycle_packing_parallel.cpp
@@ -10,6 +10,8 @@
 #include <unordered_map>
 #include <algorithm>
 #include <cassert>
+#include <thread>
+#include <mutex>
 #include <taskflow/taskflow.hpp>
 
 namespace LPMP {
@@ -17,28 +19,58 @@ namespace LPMP {
     constexpr static double tolerance = 1e-8;
 
     struct weighted_edge : public std::array<std::size_t,2> { double cost; };
-
-    void compute_connectivity(union_find& uf, graph<std::size_t>& pos_edge_index,
-        std::vector<std::atomic<double>>& pos_edge_weights) {
-        uf.reset();
-        for(std::size_t i=0; i<pos_edge_index.no_nodes(); ++i) {
-            for(auto edge_it=pos_edge_index.begin(i); edge_it!=pos_edge_index.end(i); ++edge_it) {
-                const std::size_t j = edge_it->head();
-                if(i < j) {
-                    if (pos_edge_weights[pos_edge_index.edge(i,j)] >= tolerance)
-                        uf.merge(i,j);
-                }
-            }
+    //struct atomic_weighted_edge : public std::array<std::size_t,2> { std::atomic<double> cost; };
+    struct atomic_weighted_edge {
+        std::array<size_t,2> edges;
+        std::atomic<double> cost;
+        atomic_weighted_edge(){}
+        ~atomic_weighted_edge(){}
+        atomic_weighted_edge(std::array<size_t,2> e, double c): edges(e), cost(c) {}
+        atomic_weighted_edge(size_t e1, size_t e2, double c): edges({e1, e2}), cost(c) {}
+        atomic_weighted_edge(const atomic_weighted_edge& we){ edges = we.edges; cost = we.cost.load(); }
+        atomic_weighted_edge& operator=(const atomic_weighted_edge& we){
+            edges = we.edges;
+            cost = we.cost.load();
+            return *this;
         }
+    };
+
+    template<class T>
+    class CopyableAtomic : public std::atomic<T>
+    {
+    public:
+        CopyableAtomic() :
+            std::atomic<T>(T{})
+        {}
+
+        constexpr CopyableAtomic(T desired): std::atomic<T>(desired){}
+
+        constexpr CopyableAtomic(const CopyableAtomic<T>& other): CopyableAtomic(other.load(std::memory_order_relaxed))
+        {}
+
+        CopyableAtomic& operator=(const CopyableAtomic<T>& other) {
+            this->store(other, std::memory_order_relaxed);
+            return *this;
+        }
+
+        CopyableAtomic& operator=(const T& other) {
+            this->store(other, std::memory_order_relaxed);
+            return *this;
+        }
+    };
+
+    void compute_connectivity(union_find& uf,graph<CopyableAtomic<double>>& pos_edges_graph) {
+        uf.reset();
+        pos_edges_graph.for_each_edge([&](const std::size_t i, const std::size_t j, const double cost) {
+            if(cost >= tolerance) { uf.merge(i,j); }
+        });
     };
 
     bool connected(union_find& uf, const std::size_t i, const std::size_t j) { return uf.connected(i,j); };
 
-    void cp_single(std::vector<weighted_edge>& repulsive_edges, graph<std::size_t>& pos_edge_index,
-        std::vector<std::atomic<double>>& pos_edge_weights, const std::size_t& cycle_length, std::size_t& no_nodes,
-        const bool& record_cycles, std::atomic<double>& lower_bound, cycle_packing& cp){
+    void cp_single(std::vector<weighted_edge>& repulsive_edges,graph<CopyableAtomic<double>>& pos_edges_graph, const std::size_t& cycle_length, std::size_t& no_nodes, const bool& record_cycles, std::atomic<double>& lower_bound, cycle_packing& cp){
 
-        bfs_data<graph<std::size_t>> bfs(pos_edge_index);
+        bfs_data<graph<CopyableAtomic<double>>> bfs(pos_edges_graph);
         std::size_t progress = 0;
         union_find uf(no_nodes);
 
@@ -49,12 +81,13 @@ namespace LPMP {
             // periodically update component labeling for speed-up
             progress++;
             if (progress > 0.05 * repulsive_edges.size()) {
-                compute_connectivity(uf, pos_edge_index, pos_edge_weights);
+                //std::cout<<"update component labeling\n";
+                compute_connectivity(uf, pos_edges_graph);
                 progress = 0;
             }
 
             // check if conflicted cycle exists and repulsive edge has positive weight
-            if(-re.cost <= tolerance || !connected(uf, re[0], re[1]) || std::max(re[0], re[1]) >= pos_edge_index.no_nodes()) {
+            if(-re.cost<= tolerance || !connected(uf, re[0], re[1]) || std::max(re[0], re[1]) >= pos_edges_graph.no_nodes()) {
                 const std::size_t imax = repulsive_edges.size() - 1;
                 repulsive_edges[i] = repulsive_edges[imax];
                 repulsive_edges.resize(imax);
@@ -63,8 +96,8 @@ namespace LPMP {
             }
 
             // balance short cycles as long as available and positive weight left
-            auto mask_small_edges = [&](const std::size_t i, const std::size_t j, const std::size_t edge_index, const std::size_t distance) {
-                if(pos_edge_weights[pos_edge_index.edge(i,j)] <= tolerance) return false;
+            auto mask_small_edges = [cycle_length](const std::size_t i, const std::size_t j, const double cost, const std::size_t distance) {
+                if(cost <= tolerance) return false;
                 if(distance >= cycle_length) return false;
                 return true;
             };
@@ -72,11 +105,10 @@ namespace LPMP {
 
             while(-re.cost > tolerance) {
                 double cycle_cap = std::numeric_limits<double>::infinity();
-                auto cycle_capacity = [&](const std::size_t i, const std::size_t j, const std::size_t edge_index) {
-                        cycle_cap = std::min(cycle_cap, pos_edge_weights[pos_edge_index.edge(i,j)].load()); };
-                cycle = bfs.find_path(re[0], re[1], mask_small_edges, cycle_capacity); // possibly do not reallocate for every search but give as argument to find_path
+                auto cycle_capacity = [&cycle_cap](const std::size_t i, const std::size_t j, const double cost) {
+                        cycle_cap = std::min(cycle_cap, cost); };
+                cycle = bfs.find_path(re[0], re[1], mask_small_edges, cycle_capacity); // possibly do not reallocate for every search but give as argument to find_pathatomic_weighted_edge
                 cycle_cap = std::min(cycle_cap, -re.cost);
-
                 if(cycle.size() > 0 && cycle_cap >= tolerance) {
                     if(record_cycles) cp.add_cycle(cycle.begin(), cycle.end(), cycle_cap);
 
@@ -85,28 +117,16 @@ namespace LPMP {
                     re.cost += cycle_cap;
                     assert(re.cost <= 0.0);
                     for(std::size_t c=1; c<cycle.size(); ++c) {
-                        auto idx_1 = pos_edge_index.edge(cycle[c-1],cycle[c]);
-                        auto idx_2 = pos_edge_index.edge(cycle[c],cycle[c-1]);
-                        assert(idx_1 == idx_2);
-                        pos_edge_weights[idx_1] = pos_edge_weights[idx_1] - cycle_cap;
-
-                        if(pos_edge_weights[idx_1] < 0.0){
-                            auto imax = repulsive_edges.size() - 1;
-                            for(std::size_t c_p=1; c_p <=c; ++c_p){
-                                size_t p = pos_edge_index.edge(cycle[c_p-1],cycle[c_p]);
-                                pos_edge_weights[p] = pos_edge_weights[p] + cycle_cap;
-                            }
-                            auto tmp = repulsive_edges[i];
-                            repulsive_edges[i] = repulsive_edges[imax];
-                            repulsive_edges[imax] = tmp;
-                            goto outer;
-                        }
+                        pos_edges_graph.edge(cycle[c-1], cycle[c]).store(pos_edges_graph.edge(cycle[c-1], cycle[c]).load() - cycle_cap);
+                        pos_edges_graph.edge(cycle[c], cycle[c-1]).store(pos_edges_graph.edge(cycle[c], cycle[c-1]).load() - cycle_cap);
+                        assert(pos_edges_graph.edge(cycle[c-1], cycle[c]).load() == pos_edges_graph.edge(cycle[c], cycle[c-1]).load());
                     }
 
                     lower_bound.store(lower_bound.load() + cycle_cap);
 
                     // remove repulsive edge if weight is negligible
                     if (-re.cost<= tolerance) {
+                    //    std::cout << "weight is negligible:";atomic_weighted_edge
                         auto imax = repulsive_edges.size() - 1;
                         repulsive_edges[i] = repulsive_edges[imax];
                         repulsive_edges.resize(imax);
@@ -123,8 +143,8 @@ namespace LPMP {
     cycle_packing multicut_cycle_packing_parallel_impl(const multicut_instance& input, const bool record_cycles, const int nr_threads)
     {
         const auto begin_time = std::chrono::steady_clock::now();
-        tf::Executor e1(nr_threads), e2(nr_threads);
-        tf::Taskflow tf1, tf2;
+        tf::Executor executor(nr_threads);
+        tf::Taskflow taskflow;
         cycle_packing cp;
 
         std::atomic<double> lower_bound = 0.0;
@@ -132,61 +152,56 @@ namespace LPMP {
         std::vector<weighted_edge> positive_edges;
         std::vector<std::vector<weighted_edge>> repulsive_edge_vec(nr_threads);
         std::size_t no_nodes = input.no_nodes();
+        std::vector<atomic_weighted_edge> positive_atomic_weighted_edge;
 
         auto classify_edges = [&] () {
             for (const auto& e : input.edges()){
                 if(e.cost < 0.0)
                     repulsive_edges.push_back({e[0], e[1], e.cost});
-                else if(e.cost > 0.0)
+                else if(e.cost > 0.0){
+                    std::atomic<double> atomic_cost;
+                    atomic_cost.store(e.cost);
                     positive_edges.push_back({e[0], e[1], e.cost});
+                    positive_atomic_weighted_edge.push_back({e[0], e[1], atomic_cost});
+                }
                 lower_bound = lower_bound + std::min(0.0, e.cost[0]);
             }
         };
-        auto CE = tf1.emplace(classify_edges);
+        auto CE = taskflow.emplace(classify_edges);
 
-        std::future<void> fu = e1.run(tf1);
-        fu.get();
+        graph<CopyableAtomic<double>> pos_edges_graph;
+        auto construct_pos_edges_graph = [&]() { pos_edges_graph.construct(positive_atomic_weighted_edge.begin(), positive_atomic_weighted_edge.end(),
+                [](const atomic_weighted_edge& e) -> std::array<size_t,2> { return {e.edges[0], e.edges[1]}; },
+                [](const atomic_weighted_edge& e) { return e.cost.load(); }); };
+        auto CPE = taskflow.emplace(construct_pos_edges_graph);
 
-        std::cout << "#repulsive edges = " << repulsive_edges.size() << "\n";
-        std::cout << "#attractive edges = " << positive_edges.size() << "\n";
+        CPE.gather(CE);
 
-        graph<std::size_t> pos_edge_index;
-        std::vector<std::atomic<double>> pos_edge_weights(positive_edges.size());
-        int index = 0;
-        auto construct_graph = [&]() {
-            for (auto& edge: positive_edges){
-                pos_edge_weights[index] = edge.cost;
-                index++;
-            }
-            index = 0;
-            pos_edge_index.construct(positive_edges.begin(), positive_edges.end(),
-                [&](const weighted_edge& e) { return index++; });
-        };
-        auto CPE = tf2.emplace(construct_graph);
-
-        auto RV = tf2.parallel_for(0, nr_threads, 1, [&](const std::size_t thread_no) {
+        auto RV = taskflow.parallel_for(0, nr_threads, 1, [&](const std::size_t thread_no) {
             std::size_t e = thread_no;
             while (e < repulsive_edges.size()) {
                 repulsive_edge_vec[thread_no].push_back(repulsive_edges[e]);
                 e += nr_threads;
             }
         });
+        RV.first.gather(CE);
 
-        fu = e2.run(tf2);
+        std::future<void> fu = executor.run(taskflow);
         fu.get();
 
         std::cout << "cycle packing\n";
         std::cout << "initial lower bound = " << lower_bound << "\n";
-        std::cout << "pos_edges_graph.no_nodes:" << pos_edge_index.no_nodes() << std::endl;
-        std::cout << "pos_edges_graph.no_edges:" << pos_edge_index.no_edges() << std::endl;
-
+        std::cout << "#repulsive edges = " << repulsive_edges.size() << "\n";
+        std::cout << "#attractive edges = " << positive_edges.size() << "\n";
+        std::cout << "pos_edges_graph.no_nodes:" << pos_edges_graph.no_nodes() << std::endl;
+        std::cout << "pos_edges_graph.no_edges:" << pos_edges_graph.no_edges() << std::endl;
 
         // iteratively pack cycles of given length
         const std::array<std::size_t,11> cycle_lengths = {1,2,3,4,5,6,7,8,9,10,std::numeric_limits<std::size_t>::max()};
         for(const std::size_t cycle_length : cycle_lengths) {
 
-            tf::Executor executor(nr_threads);
-            tf::Taskflow taskflow;
+            tf::Executor executor2(nr_threads);
+            tf::Taskflow taskflow2;
 
             std::cout << "find cycles of length " << cycle_length << " lower bound = " << lower_bound << "\n";
 
@@ -194,13 +209,13 @@ namespace LPMP {
             for (auto& re: repulsive_edge_vec)
                 std::random_shuffle(re.begin(), re.end());
 
-            auto CP = taskflow.parallel_for(0, nr_threads, 1, [&](const std::size_t thread_no){
+            auto CP = taskflow2.parallel_for(0, nr_threads, 1, [&](const std::size_t thread_no){
             //    std::cout << "#repulsive edges :" << repulsive_edge_vec[thread_no].size() << " remaining in thread " << thread_no << std::endl;
-                cp_single(std::ref(repulsive_edge_vec[thread_no]), std::ref(pos_edge_index), std::ref(pos_edge_weights), std::ref(cycle_length),  std::ref(no_nodes), std::ref(record_cycles), std::ref(lower_bound), std::ref(cp));
+                cp_single(std::ref(repulsive_edge_vec[thread_no]), std::ref(pos_edges_graph), std::ref(cycle_length),  std::ref(no_nodes), std::ref(record_cycles), std::ref(lower_bound), std::ref(cp));
             });
 
-            executor.run(taskflow);
-            executor.wait_for_all();
+            executor2.run(taskflow2);
+            executor2.wait_for_all();
             // terminate if no conflicted cycle remain
             size_t remain_repulsive_edges = 0;
             for (auto& re: repulsive_edge_vec) { remain_repulsive_edges += re.size(); }
