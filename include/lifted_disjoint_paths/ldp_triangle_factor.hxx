70b349fec48aa27828f89d4b0e7b17fb86353b71
Andrea <fuksova@lap-12-64.mpi-inf.mpg.de>
2020-04-16 07:42:46 +0200

    messages in triangle factor

--- include/lifted_disjoint_paths/ldp_triangle_factor.hxx
+++ include/lifted_disjoint_paths/ldp_triangle_factor.hxx
@@ -13,6 +13,7 @@
 #include <bitset>
 #include <vector>
 #include <assert.h>
+#include <unordered_map>
 namespace LPMP {
 
 class ldp_triangle_factor
@@ -20,10 +21,10 @@ class ldp_triangle_factor
 public:
 
 	//By default, all edges are lifted. However vu or uw can be base too.
-	ldp_triangle_factor(size_t vu,size_t uw,size_t vw,bool vuBase=0,bool uwBase=0): //TODO remember vertex indices instead of edge indices?
-		vuInd(vu),
-		uwInd(uw),
-		vwInd(vw)
+	ldp_triangle_factor(size_t v,size_t u,size_t w,bool vuBase=0,bool uwBase=0): //TODO remember vertex indices instead of edge indices?
+		vInd(v),
+		uInd(u),
+		wInd(w)
 {
 
 		//Feasible labelings of edge (vu,uw,vw)
@@ -41,19 +42,18 @@ public:
 		double minValue=0;
 		short minIndex=0;
 		for (short i = 0; i < labelings.size(); ++i) {
-			std::bitset<3> label=labelings[i];
+			std::bitset<3>& label=labelings[i];
 			double value=label[0]*edgeCosts[0]+label[1]*edgeCosts[1]+label[2]*edgeCosts[2];
 			if(value<minValue){
 				minValue=value;
 				minIndex=i;
 			}
 		}
-		primal_=minIndex;
 		return minValue;
 	}
 
 	double EvaluatePrimal() const{
-		std::bitset<3> label=labelings[primal_];
+		std::bitset<3>& label=labelings[primal_];
 		double value=label[0]*edgeCosts[0]+label[1]*edgeCosts[1]+label[2]*edgeCosts[2];
 		return value;
 	}
@@ -81,15 +81,54 @@ public:
 		return min1-min0;
 	}
 
+	std::unordered_map<std::pair<size_t,size_t>,double> adjustCostAndGetMessages(){
+		std::unordered_map<std::pair<size_t,size_t>,double> outputMap;
+		double d=delta(0);
+		edgeCosts[0]-=d;
+		outputMap[{vInd,uInd}]=d;
+
+		d=delta(1);
+		edgeCosts[1]-=d;
+		outputMap[{uInd,wInd}]=d;
+
+		d=delta(2);
+		edgeCosts[2]-=d;
+		outputMap[{vInd,wInd}]=d;
+
+		return outputMap;
+	}
+
 	void updateCost(size_t edgeId,double update){  //update cost of one edge, assumed indices 0-2
 		assert(edgeId<=2);
 		edgeCosts[edgeId]+=update;
 	}
 
+	void updateCost(size_t v1, size_t v2,double update){
+		size_t e=getLocalEdgeID(v1,v2);
+		updateCost(e,update);
+	}
+
+	size_t getLocalEdgeID(size_t v1, size_t v2){ //assumes that v1<v2. If edge not present in the factor, returns 3.
+		if(v1==vInd){
+			if(v2==uInd){
+				return 0;
+			}
+			else if(v2==wInd){
+				return 2;
+			}
+		}
+		else if(v1==uInd){
+			if(v2==wInd){
+				return 1;
+			}
+		}
+		return 3;
+	}
+
 private:
 	std::size_t primal_; // index of feasible labeling
-	size_t vuInd,uwInd,vwInd;
-	double edgeCosts[3];
+	size_t vInd,uInd,wInd;
+	double edgeCosts[3]; //vu, uw, vw
 	std::vector<std::bitset<3>> labelings;
 };
 
