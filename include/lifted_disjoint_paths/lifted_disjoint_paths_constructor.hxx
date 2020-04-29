fccf49e2fe1e38ed69ff118384b72ea7f5a4b793
Paul Swoboda <pswoboda@mpi-inf.mpg.de>
2020-04-29 20:57:28 +0200

    Updates to lifted disjoint paths constructor.

--- include/lifted_disjoint_paths/lifted_disjoint_paths_constructor.hxx
+++ include/lifted_disjoint_paths/lifted_disjoint_paths_constructor.hxx
@@ -25,10 +25,19 @@ namespace LPMP {
             void Tighten(const std::size_t nr_constraints_to_add);
 
         private:
+            std::size_t mcf_node_to_graph_node(std::size_t i) const;
             void prepare_mcf_costs();
 
+            std::size_t nr_nodes() const { assert(single_node_cut_factors_.size() == (mcf_->no_nodes() - 2) / 2); return single_node_cut_factors_.size(); }
+            std::size_t incoming_mcf_node(const std::size_t i) const { assert(i < nr_nodes()); return i*2; }
+            std::size_t outgoing_mcf_node(const std::size_t i) const { assert(i < nr_nodes()); return i*2+1; }
+            std::size_t mcf_source_node() const { return mcf_->no_nodes()-2; }
+            std::size_t mcf_terminal_node() const { return mcf_->no_nodes()-1; }
+            std::size_t base_graph_source_node() const { return nr_nodes() + 1; }
+            std::size_t base_graph_terminal_node() const { return nr_nodes() + 2; }
+
             LP<FMC> *lp_;
-            using mcf_solver_type = MCF::SSP<long, REAL>;
+            using mcf_solver_type = MCF::SSP<long, double>;
             std::unique_ptr<mcf_solver_type> mcf_; // minimum cost flow factor for base edges
             std::vector<std::array<SINGLE_NODE_CUT_FACTOR*,2>> single_node_cut_factors_;
     };
@@ -39,24 +48,26 @@ namespace LPMP {
     {
         // first construct minimum cost flow factor for base edges
         const std::size_t nr_base_graph_nodes = instance.getGraph().numberOfVertices() - 2;
-        const std::size_t base_graph_source = instance.getGraph().numberOfVertices() - 2;
-        const std::size_t base_graph_terminal = instance.getGraph().numberOfVertices() - 1;
+        const std::size_t base_graph_source = instance.getSourceNode();
+        const std::size_t base_graph_terminal = instance.getTerminalNode();
 
-        const std::size_t no_mcf_nodes = 2*instance.getGraph().numberOfVertices() + 2; // source/terminal vertex + 2*ordinary vertices to ensure unit capacity vertices
-        const std::size_t no_mcf_edges = 3*instance.getGraph().numberOfVertices() + instance.getGraph().numberOfEdges() + 1;
-        mcf_ = std::make_unique<mcf_solver_type>(no_mcf_nodes, no_mcf_edges);
+        const std::size_t nr_mcf_nodes = 2*nr_base_graph_nodes + 2; // source/terminal vertex + 2*ordinary vertices to ensure unit capacity vertices
+        const std::size_t nr_mcf_edges = 3*nr_base_graph_nodes + instance.getGraph().numberOfEdges() + 1; // appearance/disappearance/uniqueness edge + connection edges + source/terminal edge
+        mcf_ = std::make_unique<mcf_solver_type>(nr_mcf_nodes, nr_mcf_edges);
         
-        const std::size_t source_node = no_mcf_nodes - 2;
-        const std::size_t terminal_node = no_mcf_nodes - 1;
+        const std::size_t mcf_source_node = nr_mcf_nodes - 2;
+        const std::size_t mcf_terminal_node = nr_mcf_nodes - 1;
         auto incoming_edge = [](const std::size_t i) { return 2*i; };
         auto outgoing_edge = [](const std::size_t i) { return 2*i+1; };
+
+        mcf_->add_edge(mcf_source_node, mcf_terminal_node, 0, nr_base_graph_nodes, 0.0);
 
         // ensure unit capacity on vertices, appearance and disappearance edges
         for(std::size_t i=0; i<nr_base_graph_nodes; ++i)
         {
             mcf_->add_edge(incoming_edge(i), outgoing_edge(i), 0, 1, 0.0);
-            mcf_->add_edge(source_node, incoming_edge(i), 0, 1, 0.0);
-            mcf_->add_edge(outgoing_edge(i), terminal_node, 0, 1, 0.0);
+            mcf_->add_edge(mcf_source_node, incoming_edge(i), 0, 1, 0.0);
+            mcf_->add_edge(outgoing_edge(i), mcf_terminal_node, 0, 1, 0.0);
         }
 
         for(std::size_t i=0; i<nr_base_graph_nodes; ++i)
@@ -65,9 +76,10 @@ namespace LPMP {
             {
                 const std::size_t e = instance.getGraph().edgeFromVertex(i, ne);
                 const std::size_t j = instance.getGraph().vertexFromVertex(i, ne);
-
-                if(i < j)
+                std::cout << "base graph edge " << i << "," << j << "\n";
+                if (j != base_graph_source && j != base_graph_terminal)
                 {
+                    assert(i<j);
                     const double c = instance.getEdgeScore(e);
                     mcf_->add_edge(outgoing_edge(i), incoming_edge(j), 0, 1, c);
                 }
@@ -92,6 +104,7 @@ namespace LPMP {
     {
         prepare_mcf_costs();
         mcf_->solve();
+
         // propagate base solution to single node cut factors
     }
 
@@ -101,26 +114,59 @@ namespace LPMP {
     }
 
     template <class FACTOR_MESSAGE_CONNECTION, class MCF_FACTOR, class SINGLE_NODE_CUT_FACTOR, class MCF_SINGLE_NODE_CUT_MESSAGE>
+    std::size_t lifted_disjoint_paths_constructor<FACTOR_MESSAGE_CONNECTION, MCF_FACTOR, SINGLE_NODE_CUT_FACTOR, MCF_SINGLE_NODE_CUT_MESSAGE>::mcf_node_to_graph_node(std::size_t i) const
+    {
+        assert(i < mcf_->no_nodes());
+        if(i == mcf_source_node())
+            return base_graph_source_node();
+        if(i == mcf_terminal_node())
+            return base_graph_terminal_node();
+        else
+            return i/2; 
+    }
+            
+
+    template <class FACTOR_MESSAGE_CONNECTION, class MCF_FACTOR, class SINGLE_NODE_CUT_FACTOR, class MCF_SINGLE_NODE_CUT_MESSAGE>
     void lifted_disjoint_paths_constructor<FACTOR_MESSAGE_CONNECTION, MCF_FACTOR, SINGLE_NODE_CUT_FACTOR, MCF_SINGLE_NODE_CUT_MESSAGE>::prepare_mcf_costs()
     {
        mcf_->reset_costs();
 
-        for(std::size_t i=0; i<single_node_cut_factors_.size(); ++i)
+        for(std::size_t i=0; i<nr_nodes(); ++i)
         {
             auto *incoming_snc = single_node_cut_factors_[i][0];
             const auto incoming_min_marg = incoming_snc->get_factor()->adjustCostsAndSendMessages();
             std::cout << "incoming_min_marg size = " << incoming_min_marg.size() << "\n";
-            auto e = mcf_->first_outgoing_arc(i);
-            for (std::size_t l = 0; l < incoming_min_marg.size(); ++l, ++e)
+            //for (std::size_t l = 0; l < incoming_min_marg.size(); ++l, ++e)
+            //{
+            //    const std::size_t start_node = mcf_->tail(e);
+            //    assert(mcf_->tail(e) == i);
+            //    assert(mcf_->cost(e) == 0.0);
+            //    assert(incoming_min_marg.count(l) > 0);
+            //    mcf_->update_cost(e, incoming_min_marg.find(l)->second);
+            //}
+            std::vector<std::tuple<std::size_t,double>> incoming_min_margs_sorted;
+            for(const auto [node,m] : incoming_min_marg)
+                incoming_min_margs_sorted.push_back({node, m});
+            std::sort(incoming_min_margs_sorted.begin(), incoming_min_margs_sorted.end(), [](const auto a, const auto b) { return std::get<0>(a) < std::get<0>(b); });
+            std::size_t e = mcf_->first_outgoing_arc(incoming_mcf_node(i));
+            assert(incoming_min_margs_sorted.size() <= mcf_->no_outgoing_arcs(i));
+            for (std::size_t l = 0; l < incoming_min_margs_sorted.size(); ++l)
             {
+                std::cout << "incoming min marginal edge " << i << "," << std::get<0>(incoming_min_margs_sorted[l]) << "\n";
+                while (mcf_node_to_graph_node(mcf_->head(e)) != std::get<0>(incoming_min_margs_sorted[l]))
+                {
+                    assert(mcf_->tail(e) == i);
+                    assert(mcf_->cost(e) == 0.0);
+                    ++e;
+                }
                 const std::size_t start_node = mcf_->tail(e);
-                assert(mcf_->head(e) == i);
+                assert(mcf_->tail(e) == i);
                 assert(mcf_->cost(e) == 0.0);
-                assert(incoming_min_marg.count(l) > 0);
-                mcf_->update_cost(e, incoming_min_marg.find(l)->second);
             }
 
             auto *outgoing_snc = single_node_cut_factors_[i][1];
+            const auto outgoing_min_marg = outgoing_snc->get_factor()->adjustCostsAndSendMessages();
+            std::cout << "outgoing_min_marg size = " << outgoing_min_marg.size() << "\n";
         }
     }
 
