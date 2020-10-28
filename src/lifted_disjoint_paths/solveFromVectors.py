588d2f7b785cc86ebac61ea42ecc2938b8fdf410
Andrea <fuksova@lap-12-64.mpi-inf.mpg.de>
2020-10-28 14:59:39 +0100

    submodules updated with LifT fix

--- src/lifted_disjoint_paths/solveFromVectors.py
+++ src/lifted_disjoint_paths/solveFromVectors.py
@@ -27,8 +27,8 @@ paramsMap["MAX_TIMEGAP_COMPLETE"]="60"
 
 
 
-#pathToFiles="/home/fuksova/codes/higher-order-disjoint-paths/data/newSolverInput/"
-pathToFiles="/BS/Hornakova/nobackup/newSolverInput/"
+pathToFiles="/home/fuksova/codes/higher-order-disjoint-paths/data/newSolverInput/"
+#pathToFiles="/BS/Hornakova/nobackup/newSolverInput/"
 
 #Command line parameters of LPMP
 solverParameters=["solveFromFiles","-o",pathToFiles+"myOutputPython.txt","--maxIter","15","-v","0"]
