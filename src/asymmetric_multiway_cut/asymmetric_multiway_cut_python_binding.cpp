6580a2f1eaf1aa3f8e611fd04bf0834adaffe60d
Paul Swoboda <pswoboda@mpi-inf.mpg.de>
2020-12-07 12:12:28 +0100

    Added asymmetric multiway cut python binding.

---
+++ src/asymmetric_multiway_cut/asymmetric_multiway_cut_python_binding.cpp
@@ -0,0 +1,56 @@
+#include "asymmetric_multiway_cut/asymmetric_multiway_cut_instance.h"
+#include "asymmetric_multiway_cut/asymmetric_multiway_cut_gaec.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+
+void construct_instance(LPMP::asymmetric_multiway_cut_instance& instance, const std::vector<std::tuple<size_t,size_t,double>>& edge_costs, const py::array_t<double> node_costs)
+{
+
+    for(size_t e=0; e<edge_costs.size(); ++e)
+    {
+        const auto ec =  edge_costs[e];
+        const size_t i = std::get<0>(ec);
+        const size_t j = std::get<1>(ec);
+        const double w = std::get<2>(ec);
+        instance.edge_costs.add_edge(i, j, w);
+    }
+
+    const auto _node_costs = node_costs.unchecked<2>();
+    const size_t nr_nodes = _node_costs.shape(0);
+    const size_t nr_labels = _node_costs.shape(1);
+    std::vector<double> costs;
+    for(size_t i=0; i<nr_nodes; ++i)
+    {
+        costs.clear();
+        for(size_t l=0; l<nr_labels; ++l)
+        {
+            costs.push_back(_node_costs(i,l)); 
+        } 
+        instance.node_costs.push_back(costs.begin(), costs.end());
+    } 
+}
+
+
+PYBIND11_MODULE(asymmetric_multiway_cut_py, m) {
+    m.doc() = "python binding for LPMP asymmetric multiway cut";
+
+    py::class_<LPMP::asymmetric_multiway_cut_labeling>(m, "asymmetric_multiway_cut_labeling")
+        .def(py::init<>()); 
+
+    py::class_<LPMP::asymmetric_multiway_cut_instance>(m, "asymmetric_multiway_cut_instance")
+        .def(py::init<>())
+        .def(py::init([](const std::vector<std::tuple<size_t,size_t,double>>& edge_costs, const py::array_t<double> node_costs) {
+                    LPMP::asymmetric_multiway_cut_instance instance;
+                    construct_instance(instance, edge_costs, node_costs);
+                    return instance;
+                    }))
+        .def("evaluate", &LPMP::asymmetric_multiway_cut_instance::evaluate);
+
+        m.def("asymmetric_multiway_cut_gaec", [](const LPMP::asymmetric_multiway_cut_instance& instance) {
+                return LPMP::asymmetric_multiway_cut_gaec(instance);
+                });
+}
