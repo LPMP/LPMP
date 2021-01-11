c0178bdca375153f913bf82471f9d1f8eaf1efb5
Andrea <fuksova@lap-12-64.mpi-inf.mpg.de>
2021-01-11 07:58:27 +0100

    another commented commit

--- include/lifted_disjoint_paths/ldp_cut_factor_separator.hxx
+++ include/lifted_disjoint_paths/ldp_cut_factor_separator.hxx
@@ -5,7 +5,7 @@
 #include"ldp_min_marginals_extractor.hxx"
 #include"ldp_two_layer_graph.hxx"
 #include<list>
-#include "stable_priority_queue.hxx"
+//#include "stable_priority_queue.hxx"
 #include "ldp_factor_queue.hxx"
 
 namespace LPMP {
