1539ae62d09beb29884bd3586c4a3d4d610fdab2
pawelswoboda <pswoboda@mpi-inf.mpg.de>
2021-01-28 11:28:22 +0100

    Modified asymmetric multiway cut gaec to prefer joining small components.

--- src/asymmetric_multiway_cut/asymmetric_multiway_cut_gaec_text_input.cpp
+++ src/asymmetric_multiway_cut/asymmetric_multiway_cut_gaec_text_input.cpp
@@ -8,8 +8,11 @@ int main(int argc, char** argv)
 {
     if(argc != 2)
         throw std::runtime_error("file name expected as argument");
-    const auto instance = asymmetric_multiway_cut_parser::parse_file(argv[1]);
+    auto instance = asymmetric_multiway_cut_parser::parse_file(argv[1]);
+    std::cout << "put multicut in normal form\n";
+    instance.edge_costs.normalize();
 
+    std::cout << "Compute labeling\n";
     const auto labeling = asymmetric_multiway_cut_gaec(instance);
-    std::cout << "labeling energy = " << instance.evaluate(labeling); 
+    std::cout << "labeling energy = " << instance.evaluate(labeling) << "\n";
 }
